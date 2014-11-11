9c1e3f107ee2f0fdbb22f59be0cabec19e5a5d34
Lukas Ruebbelke <simpul@gmail.com>
2014-11-11 08:00:13 -0700

    Clean up and controller as

--- js/app.js
+++ js/app.js
@@ -1,48 +1,43 @@
 var app = angular.module('myApp', []);
 
 app.config(function ($sceProvider, $sceDelegateProvider) {
-  $sceProvider.enabled(false);
+    $sceProvider.enabled(false);
 
-  $sceDelegateProvider.resourceUrlWhitelist([
-    // Allow same origin resource loads.
-    'self',
-    // Allow loading from our assets domain.  Notice the difference between * and **.
-    'http://vimeo.com/**']);
+    $sceDelegateProvider.resourceUrlWhitelist([
+        // Allow same origin resource loads.
+        'self',
+        // Allow loading from our assets domain.  Notice the difference between * and **.
+        'http://vimeo.com/**']);
 });
 
 app.constant('URL', 'data/');
 
-app.factory('DataService', function($http, URL) {
-  var getData = function() {
-    return $http.get(URL + 'content.json');
-  };
+app.factory('DataService', function ($http, URL) {
+    var getData = function () {
+        return $http.get(URL + 'content.json');
+    };
 
-  return {
-    getData: getData
-  };
+    return {
+        getData: getData
+    };
 });
 
-app.factory('TemplateService', function($http, URL) {
-  var getTemplates = function() {
-    return $http.get(URL + 'templates.json');
-  };
+app.factory('TemplateService', function ($http, URL) {
+    var getTemplate = function (content) {
+        return $http.get('templates/' + content + '.html');
+    };
 
-  var getTemplate = function(content) {
-    return $http.get('templates/' + content + '.html');
-  };
-
-  return {
-    getTemplates: getTemplates,
-    getTemplate: getTemplate
-  };
+    return {
+        getTemplate: getTemplate
+    };
 });
 
 app.directive('contentItem', function ($compile, TemplateService) {
-    var linker = function(scope, element, attrs) {
+    var linker = function (scope, element, attrs) {
         scope.rootDirectory = 'images/';
 
-        TemplateService.getTemplate(scope.content.content_type).then(function(response) {
-            element.html(response.data).show();
+        TemplateService.getTemplate(scope.content.content_type).then(function (response) {
+            element.html(response.data);
             $compile(element.contents())(scope);
         });
     };
@@ -51,22 +46,21 @@ app.directive('contentItem', function ($compile, TemplateService) {
         restrict: "E",
         link: linker,
         scope: {
-            content:'='
+            content: '='
         }
     };
 });
 
-app.controller('ContentCtrl', function($scope, $http, DataService) {
-    "use strict";
+app.controller('ContentCtrl', function (DataService) {
+    var ctrl = this;
+    ctrl.content = [];
 
-    $scope.content = [];
-
-    $scope.fetchContent = function() {
-        DataService.getData().then(function(result){
-            $scope.content = result.data;
+    ctrl.fetchContent = function () {
+        DataService.getData().then(function (result) {
+            ctrl.content = result.data;
         });
-    }
+    };
 
-    $scope.fetchContent();
+    ctrl.fetchContent();
 });
 
