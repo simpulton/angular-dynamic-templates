7d0676dcebd1423a831af298558f721284f1d2ce
Lukas Ruebbelke <simpul@gmail.com>
2014-11-11 08:05:20 -0700

    Clean up

--- js/app.js
+++ js/app.js
@@ -1,31 +1,28 @@
 var app = angular.module('myApp', []);
 
 app.config(function ($sceProvider, $sceDelegateProvider) {
-  $sceProvider.enabled(false);
+    $sceProvider.enabled(false);
 
-  $sceDelegateProvider.resourceUrlWhitelist([
-    // Allow same origin resource loads.
-    'self',
-    // Allow loading from our assets domain.  Notice the difference between * and **.
-    'http://vimeo.com/**']);
+    $sceDelegateProvider.resourceUrlWhitelist([
+        // Allow same origin resource loads.
+        'self',
+        // Allow loading from our assets domain.  Notice the difference between * and **.
+        'http://vimeo.com/**']);
 });
 
 app.constant('URL', 'data/');
 
-app.factory('DataService', function($http, URL) {
-  var getData = function() {
-    return $http.get(URL + 'content.json');
-  };
+app.factory('DataService', function ($http, URL) {
+    var getData = function () {
+        return $http.get(URL + 'content.json');
+    };
 
-  return {
-    getData: getData
-  };
+    return {
+        getData: getData
+    };
 });
 
-app.controller('ContentCtrl', function($scope, DataService, $sce) {
-    "use strict";
-
-
+app.controller('ContentCtrl', function ($scope, DataService, $sce) {
     $scope.content = [];
     $scope.types = {};
     $scope.imageDirectory = 'images/';
@@ -33,19 +30,19 @@ app.controller('ContentCtrl', function($scope, DataService, $sce) {
 
     var format = '.html';
 
-    $scope.concat = function(directory, file) {
-      return directory + file;
+    $scope.concat = function (directory, file) {
+        return directory + file;
     };
 
-    $scope.fetchContent = function() {
-        DataService.getData().then(function(result){
+    $scope.fetchContent = function () {
+        DataService.getData().then(function (result) {
             var templateUrl = 'templates/';
 
             $scope.content = result.data;
 
             // get all content types
-            $scope.content.filter(function(item) {
-              $scope.types[item.content_type] = templateUrl + item.content_type + format;
+            $scope.content.filter(function (item) {
+                $scope.types[item.content_type] = templateUrl + item.content_type + format;
             });
         });
     };
